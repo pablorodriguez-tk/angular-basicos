4c18ab2785ccfb585224b6f65d42edb3f218932e
Pablo <pablorodriguez88@gmail.com>
2021-12-17 22:57:56 -0300

    feat: split in different components

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { MainPageComponent } from './main-page/main-page.component';
+import { PersonajesComponent } from './personajes/personajes.component';
+import { AgregarComponent } from './agregar/agregar.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, PersonajesComponent, AgregarComponent],
   exports: [MainPageComponent],
   imports: [CommonModule, FormsModule],
 })
--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-
-interface Personaje {
-  nombre: string;
-  poder: number;
-}
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-main-page',
@@ -20,19 +16,12 @@ export class MainPageComponent {
       poder: 7500,
     },
   ];
-
   nuevo: Personaje = {
     nombre: '',
     poder: 0,
   };
 
-  agregar(): void {
-    if (this.nuevo.nombre.trim().length === 0) {
-      return;
-    }
-    console.log(this.nuevo);
-
-    this.personajes.push(this.nuevo);
-    this.nuevo = { nombre: '', poder: 0 };
+  agregarNuevoPersonaje(personaje: Personaje) {
+    this.personajes.push(personaje);
   }
 }
